9933ae4b54738ebeacd04f2b5af770d1695a8c08
Jared Tobin <jared@jtobin.io>
2024-12-18 17:08:36 -0330

    meta: dep updates

--- flake.nix
+++ flake.nix
@@ -22,12 +22,19 @@
       url  = "git://git.ppad.tech/ripemd160.git";
       ref  = "master";
     };
+    ppad-secp256k1 = {
+      type = "git";
+      url  = "git://git.ppad.tech/secp256k1.git";
+      ref  = "master";
+    };
     flake-utils.follows = "ppad-sha256/flake-utils";
     nixpkgs.follows = "ppad-sha256/nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils,
-              ppad-sha256, ppad-ripemd160, ppad-bech32, ppad-base58 }:
+  outputs = {   self, nixpkgs, flake-utils
+              , ppad-sha256, ppad-ripemd160
+              , ppad-bech32, ppad-base58
+              , ppad-secp256k1 }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         lib = "ppad-btcprim";
@@ -39,17 +46,20 @@
         bech32 = ppad-bech32.packages.${system}.default;
         base58 = ppad-base58.packages.${system}.default;
         ripemd160 = ppad-ripemd160.packages.${system}.default;
+        secp256k1 = ppad-secp256k1.packages.${system}.default;
 
         hpkgs = pkgs.haskell.packages.ghc981.extend (new: old: {
           ppad-sha256 = sha256;
           ppad-bech32 = bech32;
           ppad-base58 = base58;
           ppad-ripemd160 = ripemd160;
+          ppad-secp256k1 = secp256k1;
           ${lib} = old.callCabal2nixWithOptions lib ./. "--enable-profiling" {
             ppad-sha256 = new.ppad-sha256;
             ppad-bech32 = new.ppad-bech32;
             ppad-base58 = new.ppad-base58;
             ppad-ripemd160 = new.ppad-ripemd160;
+            ppad-secp256k1 = new.ppad-secp256k1;
           };
         });
 
