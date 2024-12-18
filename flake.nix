7668179f619753dbb870b668a6ba4c7370591c80
Jared Tobin <jared@jtobin.io>
2024-12-18 15:40:29 -0330

    flake: follow sha256

--- flake.nix
+++ flake.nix
@@ -22,8 +22,8 @@
       url  = "git://git.ppad.tech/ripemd160.git";
       ref  = "master";
     };
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.follows = "ppad-sha256/flake-utils";
+    nixpkgs.follows = "ppad-sha256/nixpkgs";
   };
 
   outputs = { self, nixpkgs, flake-utils,
